da61d593b44a3700396bb160df824e921c9f6435
Casper Ng <casperngeen@gmail.com>
2024-06-04 14:51:31 +0700

    Merge main and remote

--- src/db/db.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { databaseProviders } from './db.providers';
-
-// to allow the rest of the app to access
-@Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
-})
-export class DatabaseModule {}
