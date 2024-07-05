ae1b188bc2d783da79c09959c192790dde44ddec
Casper Ng <casperngeen@gmail.com>
2024-07-05 17:32:52 +0700

    Disabled react strict mode to prevent double rendering

--- frontend-portal/next.config.mjs
+++ frontend-portal/next.config.mjs
@@ -1,7 +1,8 @@
-import path from 'path';
+// import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: false,
     webpack: (config) =>
         {
             // config.resolve.alias = {
